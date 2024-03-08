0aecb60cd26971df5bbb6dd454f7920d41ba23d3
hieunguyen <duyhieu0212@gmail.com>
2024-03-08 10:41:06 +0700

    fix 3 errors in pull 1

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+import "./custom/jquery-3.7.1.min.js";
+import "./custom/bootstrap.bundle.min.js";
 import "./custom/main";
