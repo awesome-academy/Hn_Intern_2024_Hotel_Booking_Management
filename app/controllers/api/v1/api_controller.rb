51aeb51ab74b11388e8505d83ed12ff60e0aa8ab
hieunguyen <duyhieu0212@gmail.com>
2024-05-02 09:10:00 +0700

    Api for booking&room controllers

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,13 @@
 module Api::V1
   class ApiController < ActionController::API
+    include Pagy::Backend
+
+    def authenticate_user
+      token, _options = token_and_options(request)
+      user_id = AuthenticationTokenService.decode(token)
+      @user ||= User.find(user_id)
+    rescue StandardError => e
+      render json: {"error" => e.message}, status: :unauthorized
+    end
   end
 end
